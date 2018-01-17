a4b58f9818aacca6aa92e8b0534469f36c0d90b9
Asa Baylus <asa@baylus.com>
2018-01-17 09:22:49 -0500

    update build

--- dist/main.js
+++ dist/main.js
@@ -6500,6 +6500,9 @@ var CommandPalette = function (_React$Component) {
       var filterOptions = {
         shouldSort: true,
         includeMatches: true,
+        threshold: 0.6,
+        location: 10,
+        distance: 100,
         maxPatternLength: 32,
         minMatchCharLength: 1,
         keys: ["name", "section"]
