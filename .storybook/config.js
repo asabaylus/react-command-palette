316fdbf94265beb9d27623a75fc2a4c4b27ffbf5
Asa Baylus <asa@baylus.com>
2019-01-08 12:57:01 -0500

    add chromatic

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,5 @@
 import { configure } from '@storybook/react';
+import 'storybook-chromatic';
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /.stories.js$/);
