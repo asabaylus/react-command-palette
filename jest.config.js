f64a007af09a392b01fe99a18f1242a83e2edd54
Asa Baylus <asa@baylus.com>
2019-03-09 18:11:56 -0500

    fix coverage

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  testURL: "http://localhost/",
   snapshotSerializers: ["./node_modules/enzyme-to-json/serializer"],
   moduleNameMapper: {
-    "\\.(css|less|sass|scss)$": "<rootDir>/src/__mocks__/styleMock.js",
+    "\\.(css|less|sass|scss)$": "<rootDir>/src/__mocks__/styleMock.js"
   },
   collectCoverage: true,
   coverageDirectory: "coverage",
-  collectCoverageFrom: ["**/src/*.{js}", "!**/node_modules/**", "!src/main.js"],
-  coverageReporters: ["lcov", "text"]
-}
+  collectCoverageFrom: ["src/*.js"],
+  coveragePathIgnorePatterns: ["node_modules", "src/main.js"],
+  coverageReporters: ["json", "lcov", "text"]
+};
