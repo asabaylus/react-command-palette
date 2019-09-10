0f41f08e64ec055fa4d17991cd9832fb7b8f09d6
Asa Baylus <asa@baylus.com>
2019-09-09 20:40:39 -0400

    bump storybook to 5.2 rc

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,7 +2,7 @@ import { configure } from "@storybook/react";
 import "storybook-chromatic";
 
 // automatically import all files ending in *.stories.js
-const req = require.context("../stories", true, /.stories.js$/);
+const req = require.context("../stories", true, /\.stories\.(js|jsx|ts|tsx|mdx)$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
