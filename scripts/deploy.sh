c79da0e300353209c0dec464b1bd65b0e8816b30
Asa Baylus <asa@baylus.com>
2020-02-07 10:13:46 -0500

    add deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Deploying command palette..."
+
+if [ "$CI_BRANCH" != "master" ];
+then
+  echo "Exit: branch is not git master"
+  exit
+else
+  npm version preminor
+  git push
+  git push --tags
+  npm publish --dry-run
+fi
