562c87696aaab9374a2c282b7388d80564b5d1e5
Asa Baylus <asa@baylus.com>
2020-05-16 01:52:48 -0400

    bump chromatic

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,5 @@
 import { configure } from "@storybook/react";
-import "storybook-chromatic";
+import "chromatic";
 
 // automatically import all files ending in *.stories.js
 const req = require.context("../stories", true, /.stories.js$/);
