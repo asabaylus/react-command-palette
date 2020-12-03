99d077e9d06a6760283f7e4273709325be68d008
Asa Baylus <asa@baylus.com>
2020-12-03 15:01:41 -0500

    bump versions

--- src/command-palette.shouldFocusAfterRender.test.js
+++ src/command-palette.shouldFocusAfterRender.test.js
@@ -6,7 +6,7 @@
 
 import React from "react";
 import Enzyme, { mount } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 import CommandPalette from "./command-palette";
 import mockCommands from "./__mocks__/commands";
 
