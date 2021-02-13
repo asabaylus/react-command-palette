074ab5bfb3e471e60eea5ada5bf730f623c360d9
Asa Baylus <asa@baylus.com>
2021-02-13 14:42:03 -0500

    add preset

--- .storybook/main.js
+++ .storybook/main.js
@@ -12,6 +12,6 @@ module.exports = {
     },
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    // "@storybook/preset-create-react-app"
+    "@storybook/preset-create-react-app"
   ]
 }
