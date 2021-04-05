249f7254adb9a8518fab9b9f6621aeb30f85979c
Asa Baylus <asa@baylus.com>
2021-04-05 00:31:10 -0400

    make CSS work with Nextjs, Closes #602

---
+++ rollup.config.js
@@ -0,0 +1,64 @@
+// Rollup plugins
+import babel from "rollup-plugin-babel";
+import commonjs from "rollup-plugin-commonjs";
+import resolve from "rollup-plugin-node-resolve";
+import postcss from "rollup-plugin-postcss";
+import replace from "rollup-plugin-replace";
+import pkg from "./package.json";
+
+
+const plugins = [
+  replace({
+    "process.env.NODE_ENV": JSON.stringify("production"),
+  }),
+  postcss(),
+  commonjs({
+    include: ["node_modules/**"],
+    exclude: ["node_modules/process-es6/**"],
+    namedExports: {
+      "node_modules/react/index.js": [
+        "Children",
+        "Component",
+        "PropTypes",
+        "createElement",
+        "isValidElement",
+        "cloneElement",
+        "createRef",
+      ],
+      "node_modules/react-dom/index.js": ["render"],
+    },
+  }),
+  babel({
+    exclude: "node_modules/**",
+    runtimeHelpers: true,
+  }),
+  resolve({
+    jsnext: true,
+    main: true,
+    browser: true,
+  }),
+];
+
+export default {
+  input: "src/index.js",
+  external: ["react", "react-dom"],
+  output: [
+    {
+      file: pkg.main,
+      format: "umd",
+      globals: {
+        react: "React",
+        "react-dom": "ReactDOM",
+      },
+      sourcemap: "external",
+      name: "CommandPalette",
+      exports: "named",
+    },
+    {
+      file: pkg.module,
+      format: "es",
+      sourcemap: "external",
+    },
+  ],
+  plugins,
+};
